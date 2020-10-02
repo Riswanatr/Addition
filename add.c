ec4db69fd05370760db758c07a88b51bfc3e7c9e
Vishal dnyaneshwar Sawai <67541577+VishalSawai0@users.noreply.github.com>
2020-10-02 22:34:42 +0530

    Update add.c

--- add.c
+++ add.c
@@ -6,12 +6,12 @@ int main()
 {
    
     int a,b;
-
+    printf("\n------------------Addition------------------");
     printf("Enter two numbers");
  
     scanf("%d%d",&a,&b);
     
-    printf("The sum is : %d",a+b);
+    printf("The Addition is two =: %d",a+b);
 
 
     return 0;
